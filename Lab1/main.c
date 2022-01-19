a3f7441440165f82cacc376daa3237d246b09905
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-01-18 19:20:14 -0700

    More fixessssssssssss
    * Inline stuff in serial.c
    * Check for square p earlier in the program
    * free thread_handles array after join

--- Lab1/main.c
+++ Lab1/main.c
@@ -24,17 +24,11 @@ int n, thread_count, sqrt_p;
 int **A, **B, **C;
 double total;
 
-void init_result_matrix();
-
 void *multiply_thread(void *rank) {
-    /*
-    Multiplies the matrices A and B into matrix C
-    📋 TODO: Implement using shared memory and pthreads
-    */
     long my_rank;
-    int i, j, r;
-    int x, y;
-    int first_i, last_i, first_j, last_j;
+    int i, j, r, x, y;
+    int first_i, last_i, first_j, last_j, last_r;
+
     my_rank = (long)rank;
     x = my_rank / sqrt_p;
     y = my_rank % sqrt_p;
@@ -43,54 +37,59 @@ void *multiply_thread(void *rank) {
     last_i = (n / sqrt_p) * (x + 1) - 1;
     first_j = (n / sqrt_p) * y;
     last_j = (n / sqrt_p) * (y + 1) - 1;
+    last_r = n - 1;
 
     for (i = first_i; i <= last_i; i++)
         for (j = first_j; j <= last_j; j++) {
             C[i][j] = 0;
-            for (r = 0; r <= n - 1; r++)
+            for (r = 0; r <= last_r; r++)
                 C[i][j] += A[i][r] * B[r][j];
         }
 
     return NULL;
 }
 
-void print_usage(const char *prog_name) {
-    fprintf(stderr, "USAGE: %s <thread_count>\n", prog_name);
-    exit(1);
-}
-
 void do_experiment() {
     double start, end;
-    long thread;
+    long i;
     pthread_t *thread_handles;
     if (Lab1_loadinput(&A, &B, &n) != 0) exit(1);
 
-    if (n * n % thread_count != 0 || sqrt_p * sqrt_p != thread_count) {
-        fprintf(stderr, "Not a valid p!");
+    if ((n * n) % thread_count != 0) {
+        fprintf(stderr, "Thread count is not a factor of (matrix size)^2!\n");
         exit(1);
     }
 
-    init_result_matrix();
+    C = malloc(n * sizeof(int *));
+    for (i = 0; i < n; i++)
+        C[i] = malloc(n * sizeof(int));
+
     GET_TIME(start);
-
     thread_handles = malloc(thread_count * sizeof(pthread_t));
-
-    for (thread = 0; thread < thread_count; ++thread)
-        pthread_create(&thread_handles[thread], NULL, multiply_thread,
-                       (void *)thread);
-
-    for (thread = 0; thread < thread_count; ++thread)
-        pthread_join(thread_handles[thread], NULL);
-
+    for (i = 0; i < thread_count; ++i)
+        pthread_create(&thread_handles[i], NULL, multiply_thread, (void *)i);
+    for (i = 0; i < thread_count; ++i)
+        pthread_join(thread_handles[i], NULL);
     GET_TIME(end);
+
     total = end - start;
+    free(thread_handles);
     if (Lab1_saveoutput(C, &n, total) != 0) exit(1);
 }
 
 int main(int argc, const char **argv) {
-    if (argc != 2) print_usage(argv[0]);
+    double sqrt_p_double;
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: %s <thread_count>\n", argv[0]);
+        exit(1);
+    };
     thread_count = atoi(argv[1]);
-    sqrt_p = sqrt(thread_count);
+    sqrt_p_double = sqrt(thread_count);
+    if (sqrt_p_double != (int)sqrt_p_double) {
+        fprintf(stderr, "Thread count is not a square!\n");
+        exit(1);
+    }
+    sqrt_p = sqrt_p_double;
 
 #ifdef DEV
     say_hello();
@@ -104,14 +103,4 @@ int main(int argc, const char **argv) {
 #endif
 
     return 0;
-}
-
-void init_result_matrix() {
-    /*
-    Allocate memory for the result matrix
-    */
-    int i;
-    C = malloc(n * sizeof(int *));
-    for (i = 0; i < n; i++)
-        C[i] = malloc(n * sizeof(int));
 }
--- Lab1/serial.c
+++ Lab1/serial.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "lab1_IO.h"
 
-#include <sys/time.h>
-
-/* Extracted from timer.h in the Development kit */
-#define GET_TIME(now)                                                          \
-    {                                                                          \
-        struct timeval t;                                                      \
-        gettimeofday(&t, NULL);                                                \
-        now = t.tv_sec + t.tv_usec / 1000000.0;                                \
-    }
-
-int n;
-int **A, **B, **C;
-double total;
-
 int main(int argc, const char **argv) {
+    int n;
+    int **A, **B, **C;
     int i, j, r;
-    double start, end;
+
+    struct timeval t;
+    double start, end, total;
+
     if (Lab1_loadinput(&A, &B, &n) != 0) exit(1);
 
     C = malloc(n * sizeof(int *));
     for (i = 0; i < n; i++)
         C[i] = malloc(n * sizeof(int));
-    GET_TIME(start);
 
+    gettimeofday(&t, NULL);
+    start = t.tv_sec + t.tv_usec / 1000000.0;
+    /* Start timed section */
     for (i = 0; i < n; i++)
         for (j = 0; j < n; j++) {
             C[i][j] = 0;
-            for (r = 0; r <= n - 1; r++)
+            for (r = 0; r < n; r++)
                 C[i][j] += A[i][r] * B[r][j];
         }
+    /* End timed section */
+    gettimeofday(&t, NULL);
+    end = t.tv_sec + t.tv_usec / 1000000.0;
 
-    GET_TIME(end);
     total = end - start;
+
     printf("Total running time: %f seconds\n", total);
     if (Lab1_saveoutput(C, &n, total) != 0) exit(1);
     return 0;
