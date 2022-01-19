ee70a6aafcbfff6181066b3c97c8e746bff30fc3
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-01-18 17:43:31 -0700

    Finish lab 1??

---
+++ Lab1/serial.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lab1_IO.h"
+
+#include <sys/time.h>
+
+/* Extracted from timer.h in the Development kit */
+#define GET_TIME(now)                                                          \
+    {                                                                          \
+        struct timeval t;                                                      \
+        gettimeofday(&t, NULL);                                                \
+        now = t.tv_sec + t.tv_usec / 1000000.0;                                \
+    }
+
+int n;
+int **A, **B, **C;
+double total;
+
+int main(int argc, const char **argv) {
+    int i, j, r;
+    double start, end;
+    Lab1_loadinput(&A, &B, &n);
+
+    C = malloc(n * sizeof(int *));
+    for (i = 0; i < n; i++)
+        C[i] = malloc(n * sizeof(int));
+    GET_TIME(start);
+
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++) {
+            C[i][j] = 0;
+            for (r = 0; r <= n - 1; r++)
+                C[i][j] += A[i][r] * B[r][j];
+        }
+
+    GET_TIME(end);
+    total = end - start;
+    Lab1_saveoutput(C, &n, total);
+    return 0;
+}
