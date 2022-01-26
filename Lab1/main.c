09257b1a2df9355cfd5df92884d911c01a279d77
Patricia Zafra <pzafra@ualberta.ca>
2022-01-25 20:00:13 -0700

    Add comments

--- Lab1/main.c
+++ Lab1/main.c
@@ -18,10 +18,15 @@ int **A, **B, **C, n, thread_count, sqrt_p;
 
 void *multiply_thread(void *rank) {
     int i, j, r, x, y, block;
+
+    // set x and y per specs
     x = (int)floor((long)rank / sqrt_p);
     y = (long)rank % sqrt_p;
+
     block = (n / sqrt_p);
 
+    // lower boundary: block * var
+    // upper boundary: block * (var + 1)
     for (i = block * x; i < block * (x + 1); i++)
         for (j = block * y; j < block * (y + 1); j++)
             for (r = 0; r < n; r++)
@@ -30,11 +35,13 @@ void *multiply_thread(void *rank) {
     return NULL;
 }
 
+/* main program thread */
 double do_experiment() {
     double start, end;
     long i, j;
     pthread_t *thread_handles;
 
+    // allocate memory for resultant matrix
     C = malloc(n * sizeof(*C));
     for (i = 0; i < n; i++) {
         C[i] = malloc(n * sizeof(*C[i]));
@@ -42,18 +49,28 @@ double do_experiment() {
             C[i][j] = 0;
     }
 
+    // start timer
     GET_TIME(start);
     thread_handles = malloc(thread_count * sizeof(*thread_handles));
+
     for (i = 0; i < thread_count; ++i)
         pthread_create(&thread_handles[i], NULL, multiply_thread, (void *)i);
     for (i = 0; i < thread_count; ++i)
         pthread_join(thread_handles[i], NULL);
+
+    // free dynamically allocated memory
     free(thread_handles);
+    for (i = 0; i < n; i++)
+        free(C[i]);
+    free(C);
+
+    // stop the timer
     GET_TIME(end);
 
     return end - start;
 }
 
+/* checks if thread count is non-zero, a perfect square, and a factor of n^2 */
 void usage(const char *prog_name) {
     fprintf(stderr,
             "USAGE: %s p\np is the thread count which must be >=1, "
@@ -62,22 +79,31 @@ void usage(const char *prog_name) {
     exit(1);
 }
 
+/* main program */
 int main(int argc, const char **argv) {
     double sqrt_p_double, total;
 
+    // check for proper usage of program
     if (argc != 2) usage(argv[0]);
+
+    // check thread count is non-zero
     thread_count = atoi(argv[1]);
     if (thread_count == 0) usage(argv[0]);
 
+    // check thread count is a perfect square
     sqrt_p_double = sqrt(thread_count);
     if (sqrt_p_double != (int)sqrt_p_double) usage(argv[0]);
     sqrt_p = sqrt_p_double;
 
     if (Lab1_loadinput(&A, &B, &n) != 0) return 1;
+
+    // check thread count is a factor of n^2
     if ((n * n) % thread_count != 0) usage(argv[0]);
 
+    // start experiment and get time it takes to execute
     total = do_experiment();
     printf("Total running time: %f seconds\n", total);
+
     if (Lab1_saveoutput(C, &n, total) != 0) return 1;
     return 0;
 }
