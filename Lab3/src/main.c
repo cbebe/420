b34cbd5db8752c4d07c51e718f09266faed03da4
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-02-20 00:30:25 -0700

    Setup Lab3 project
    This is probably kinda dirty... still getting used to Spacemacs lmao

---
+++ Lab3/src/main.c
@@ -0,0 +1,21 @@
+
+#include "Lab3IO.h"
+#include "timer.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  double **A, *x, start, end;
+  int size;
+
+  Lab3LoadInput(&A, &size);
+  GET_TIME(start);
+
+  x = malloc(sizeof(*x) * size);
+  printf("Hello world!\n");
+
+  GET_TIME(end);
+
+  Lab3SaveOutput(x, size, end - start);
+}
