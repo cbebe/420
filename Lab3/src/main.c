feb8ceacec2e8849a3c9b0254405ca5a3702cda7
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-03-06 19:13:36 -0700

    Parallelize main function (#39)
    Also return total time from `solve` function

--- Lab3/src/main.c
+++ Lab3/src/main.c
@@ -2,14 +2,14 @@
 #include "solver.h"
 #include "timer.h"
 
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define USAGE(prog_name)                                                                           \
-    do {                                                                                           \
-        fprintf(stderr, "USAGE: %s <NUM_THREADS>\n", prog_name);                                   \
-        exit(1);                                                                                   \
-    } while (0)
+inline void usage(const char *prog_name) {
+    fprintf(stderr, "USAGE: %s <NUM_THREADS>\n", prog_name);
+    exit(1);
+}
 
 double **A, *X;
 /* My compiler complains about redefining the built-in function index as a non-function so we had to
@@ -33,43 +33,48 @@ int *index_vec, size, num_threads;
  *         /,_|  |   /,_/   /
  *            /,_/      '`-'
  */
-void solve() {
+double solve() {
     int i;
+    double start, end;
 
+    GET_TIME(start);
     /* Create answer vector and early return for basic case where size == 1 */
     X = CreateVec(size);
     if (size == 1) {
         X[0] = A[0][1] / A[0][0];
-        return;
+        GET_TIME(end);
+        return end - start;
     }
 
     /* Create index vector */
     index_vec = malloc(size * sizeof(*index_vec));
+#pragma omp parallel for num_threads(num_threads)
     for (i = 0; i < size; ++i)
         index_vec[i] = i;
 
     gaussian();
     jordan();
 
-    /* solution */
+/* solution */
+#pragma omp parallel for num_threads(num_threads)
     for (i = 0; i < size; ++i)
         X[i] = A[index_vec[i]][size] / A[index_vec[i]][i];
+    GET_TIME(end);
+
+    return end - start;
 }
 
 int main(int argc, const char **argv) {
-    double start, end;
-    if (argc != 2) USAGE(argv[0]);
+    double time;
 
+    if (argc != 2) usage(argv[0]);
     num_threads = atoi(argv[1]);
-    if (!num_threads) USAGE(argv[0]);
+    if (!num_threads) usage(argv[0]);
 
     Lab3LoadInput(&A, &size);
-    GET_TIME(start);
-    solve(A, size);
-    GET_TIME(end);
-
-    printf("Time taken: %e seconds\n", end - start);
-    Lab3SaveOutput(X, size, end - start);
+    time = solve();
+    printf("Time taken: %e seconds\n", time);
+    Lab3SaveOutput(X, size, time);
     DestroyVec(X);
     DestroyMat(A, size);
 
