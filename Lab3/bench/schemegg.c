d65cee54fe759161e8795e7b37ea9b8f2f978a80
Patricia Zafra <pzafra@ualberta.ca>
2022-03-08 12:47:28 -0700

    Change scheme gg

--- Lab3/bench/schemegg.c
+++ Lab3/bench/schemegg.c
@@ -9,52 +9,47 @@
  *
  */
 #include "solver.h"
+#include "util.h"
 #include <omp.h>
-
-#define SWAP(a, b)                                                                                 \
-    do {                                                                                           \
-        int temp = a;                                                                              \
-        a = b;                                                                                     \
-        b = temp;                                                                                  \
-    } while (0)
 
 /* Gaussian elimination */
 void gaussian() {
     double temp;
     int i, j, k;
+    int max = 0;
 
-    for (k = 0; k < size - 1; ++k) {
+    for (k = 0; k < size - 1; k++) {
         /* Pivoting */
-        temp = j = 0;
-#pragma omp parallel for num_threads(thread_count) schedule(guided)
-        for (i = k; i < size; ++i)
-            if (temp < A[index_vec[i]][k] * A[index_vec[i]][k]) {
-                temp = A[index_vec[i]][k] * A[index_vec[i]][k];
-                j = i;
-            }
+        /* Use single thread when finding max and swapping */
+        /* clang-format off */
+        #pragma omp single
+        {
+            max = pivot(k);
+            swap(index_vec[k], index_vec[max]);
+        }
 
-        if (j != k) SWAP(index_vec[j], index_vec[k]);
-
-            /* calculating */
-#pragma omp parallel for num_threads(thread_count) schedule(guided)
-        for (i = k + 1; i < size; ++i) {
+        /* Elimination */
+        /* Parallelize elimination steps */
+        #pragma omp parallel for num_threads(thread_count) schedule(guided)
+        for (i = k + 1; i < size; i++) {
             temp = A[index_vec[i]][k] / A[index_vec[k]][k];
-            for (j = k; j < size + 1; ++j)
+            for (j = k; j < size + 1; j++)
                 A[index_vec[i]][j] -= A[index_vec[k]][j] * temp;
         }
+        /* clang-format on */
     }
 }
 
 /* Jordan elimination */
 void jordan() {
-    double temp;
     int i, k;
-    for (k = size - 1; k > 0; --k) {
-#pragma omp parallel for num_threads(thread_count) schedule(guided)
-        for (i = k - 1; i >= 0; --i) {
-            temp = A[index_vec[i]][k] / A[index_vec[k]][k];
-            A[index_vec[i]][k] -= temp * A[index_vec[k]][k];
-            A[index_vec[i]][size] -= temp * A[index_vec[k]][size];
+
+    for (k = size - 1; k > 0; k--) {
+        #pragma omp parallel for num_threads(thread_count) schedule(guided)
+        for (i = k - 1; i >= 0; i--) {
+            A[index_vec[i]][size] -=
+                A[index_vec[i]][k] / A[index_vec[k]][k] * A[index_vec[k]][size];
+            A[index_vec[i]][k] = 0;
         }
     }
 }
