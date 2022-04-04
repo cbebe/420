628022d37208cae4398714f873cd44a0ad40be3e
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-04-04 09:26:46 -0600

    Use RDD for Task 1 as well
    This scales better for larger N despite the slow startup

--- Lab5/shell/task1.scala
+++ Lab5/shell/task1.scala
@@ -6,16 +6,8 @@
   * This is found by counting the number of occurrences of a number as a
   * destination node, i.e. as the second element in a line
   */
-import scala.io.Source
-val result = Source.fromFile("data_input").getLines.map {
-    line => line.split("\t")(1).toInt
-}.toList.groupBy(identity).mapValues(_.size).maxBy(_._2)._2
-
-// Alternative solution using RDD
-/*
 val result = sc.textFile("data_input").map { line =>
     line.split("\t")(1).toInt
 }.groupBy(identity).mapValues(_.size).reduce((acc, value) => {
     if (acc._2 < value._2) value else acc
 })._2
-*/
