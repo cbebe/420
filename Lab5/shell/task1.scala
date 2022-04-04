c70dd3676cd0f038ad769c23254bfeff41a5eb1e
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-04-04 09:16:03 -0600

    Add alternative Task 1 solution using RDD
    Might be a good example for Task 3

--- Lab5/shell/task1.scala
+++ Lab5/shell/task1.scala
@@ -10,3 +10,12 @@ import scala.io.Source
 val result = Source.fromFile("data_input").getLines.map {
     line => line.split("\t")(1).toInt
 }.toList.groupBy(identity).mapValues(_.size).maxBy(_._2)._2
+
+// Alternative solution using RDD
+/*
+val result = sc.textFile("data_input").map { line =>
+    line.split("\t")(1).toInt
+}.groupBy(identity).mapValues(_.size).reduce((acc, value) => {
+    if (acc._2 < value._2) value else acc
+})._2
+*/
