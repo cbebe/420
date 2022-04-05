b2dbb99e1d1d3a7e2b603f75a1fe087b842d9e16
Patricia Zafra <45130137+99patricia@users.noreply.github.com>
2022-04-04 21:28:50 -0600

    Do tasks 3 and 4 (#68)
    * do task 3
    
    * Add task 4
    
    * remove println whoops)

--- Lab5/shell/task3.scala
+++ Lab5/shell/task3.scala
@@ -1,1 +1,15 @@
-
+/** task3.txt
+  * Return an RDD named “result” containing the node ID(s) which have the
+  * largest outdegree.
+  *
+  * This was done by counting the outlinks of each node, then grouping the
+  * node IDs which have the max number of outlinks, then return all node IDs if more than
+  * one node has the same max outdegree.
+  */
+val nodeIds = sc.textFile("data_input").map { line =>
+    line.split("\t")(0).toInt }
+val counts = nodeIds.groupBy(identity).mapValues(_.size)
+val maxVal = counts.values.max
+val maxPairs = counts.filter{case (_, v) => v == maxVal }
+val IdsList = maxPairs.collect().map(_._1).toList
+val result = sc.parallelize(IdsList)
--- Lab5/shell/task4.scala
+++ Lab5/shell/task4.scala
@@ -1,1 +1,16 @@
-
+/** task4.txt
+  * Return a key-value pair RDD named “result” containing BOTH all the
+  * original one-hop links and all the two-hop links, in which each key stores the source
+  * node of a link and the corresponding value stores the destination node of the link.
+  */
+val pairs = sc.textFile("data_input").flatMap { line =>
+    val pair = line.split("\t").map(n => n.toInt)
+    Map(pair(0) -> pair(1))
+}
+val cartesian = pairs.cartesian(pairs)
+val twoHop = cartesian.filter {
+    case ((n1, d1), (n2, d2)) => ((d1 == n1) || (n1 != n2)) && (d1 == n2)
+}.map {
+    case ((n1, d1), (n2, d2)) => (n1, d2)
+}
+val result = pairs ++ twoHop
