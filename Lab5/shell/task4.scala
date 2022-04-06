f02cdbd761ccf6021851e5b8997a956941748489
Patricia Zafra <pzafra@ualberta.ca>
2022-04-06 09:15:55 -0600

    Add comment for task4

--- Lab5/shell/task4.scala
+++ Lab5/shell/task4.scala
@@ -2,6 +2,11 @@
   * Return a key-value pair RDD named “result” containing BOTH all the
   * original one-hop links and all the two-hop links, in which each key stores the source
   * node of a link and the corresponding value stores the destination node of the link.
+  *
+  * Finding the two-hop links was done by taking the cartesian product of the original pairs
+  * with itself, then filter it by a condition that finds the (node, destination) pairs of the
+  * destination of the original node. Then, the two-hop links are unioned with the one-hop links
+  * to get the final result.
   */
 val pairs = sc.textFile("data_input").flatMap { line =>
     val pair = line.split("\t").map(n => n.toInt)
